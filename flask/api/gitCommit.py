d606d30f51242d84386fcb3e4c92f9e3cb8fc539
TimmyHung <timmy0707@gmail.com>
2024-06-12 16:56:55 +0000

    新增擷取GitHub commit的API 優化測試資料格式

---
+++ flask/api/gitCommit.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+from chromaDB import loadData, query
+import requests
+import os
+
+git_blueprint = Blueprint('gitCommit', __name__)
+
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+REPO_OWNER = "TimmyHung"
+REPO_NAME = "Graduation-Project"
+
+@git_blueprint.route('/api/gitCommit', methods=['GET'])
+def get_commits():
+    url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/commits'
+    headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        commits = response.json()
+        commit_data = [{'id': commit['sha'][:6], 'author': commit['commit']['author']['name'], 'avatarURL': commit['author']['avatar_url'], 'commitURL': commit['html_url'], 'date': commit['commit']['author']['date'], 'message': commit['commit']['message']} for commit in commits]
+        return jsonify({'status': True, 'message': 'Success', 'commit': commit_data}), 200
+    except Exception as e:
+        return str(e), 500
--- flask/main.py
+++ flask/main.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from api.register import register_blueprint
 from api.login import login_blueprint
-from api.generateQuery import query_blueprint
+# from api.generateQuery import query_blueprint
 from api.jwt_verify import jwt_verify_blueprint
 from api.discordCallback import discord_callback_blueprint
+from api.gitCommit import git_blueprint
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://timmyhung.pettw.online"}})
@@ -14,9 +17,10 @@ load_dotenv()
 # Register blueprints
 app.register_blueprint(register_blueprint)
 app.register_blueprint(login_blueprint)
-app.register_blueprint(query_blueprint)
 app.register_blueprint(jwt_verify_blueprint)
 app.register_blueprint(discord_callback_blueprint)
+app.register_blueprint(git_blueprint)
+# app.register_blueprint(query_blueprint)
 
 # Default route
 @app.route('/', methods=['GET'])
