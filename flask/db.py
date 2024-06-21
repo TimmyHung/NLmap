6472e1f28c716bd3cba29e2c0a4e9bf73de41927
TimmyHung <timmy0707@gmail.com>
2024-06-21 22:04:58 +0000

    修正資料庫連線休眠問題 修正開發日誌倒數日時間錯誤的問題

--- flask/db.py
+++ flask/db.py
@@ -1,5 +1,7 @@
 import os
 import pymysql
+import threading
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,3 +16,16 @@ db = pymysql.connect(
 
 def get_db_cursor():
     return db.cursor()
+
+def keep_alive(connection):
+    while True:
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1")
+            connection.commit()
+        except pymysql.MySQLError as e:
+            print(f"[MYSQL]Keep-alive query failed: {e}")
+        time.sleep(21600) # 每小時發送一次，確保SQL連線不會被關閉
+
+# 啟動keep_alive線程
+threading.Thread(target=keep_alive, args=(db,), daemon=True).start()
