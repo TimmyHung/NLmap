314bd12478ace8f5e53e9208e74aebdaf0f3e4d4
MaxiLeung <maxileung666@gmail.com>
2024-08-07 18:49:38 +0800

    描述你的修改

--- flask/api/gitCommit.py
+++ flask/api/gitCommit.py
@@ -19,16 +19,7 @@ def get_commits():
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         commits = response.json()
-        commit_data = [
-            {
-                'id': commit['sha'][:6],
-                'author': commit['commit']['author']['name'],
-                'avatarURL': commit['author']['avatar_url'],
-                'commitURL': commit['html_url'],
-                'date': commit['commit']['author']['date'],
-                'message': commit['commit']['message']
-            } for commit in commits
-        ]
+        commit_data = [{'id': commit['sha'][:6], 'author': commit['commit']['author']['name'], 'avatarURL': commit['author']['avatar_url'], 'commitURL': commit['html_url'], 'date': commit['commit']['author']['date'], 'message': commit['commit']['message']} for commit in commits]
         return jsonify({'status': True, 'message': 'Success', 'commit': commit_data}), 200
     except requests.exceptions.HTTPError as http_err:
         return jsonify({'status': False, 'message': f'HTTP error occurred: {http_err}'}), 500
@@ -36,8 +27,3 @@ def get_commits():
         return jsonify({'status': False, 'message': f'Request error occurred: {req_err}'}), 500
     except Exception as e:
         return jsonify({'status': False, 'message': f'An error occurred: {str(e)}'}), 500
-
-
-
-
-
