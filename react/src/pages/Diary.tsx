2e98670d51e4efbea4aac5405bad402565ab825a
TimmyHung <timmy0707@gmail.com>
2024-09-02 03:32:19 +0800

    調整開發日誌介面並更改為Tailwind CSS。

--- react/src/pages/Diary.tsx
+++ react/src/pages/Diary.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '@/store';
-import css from '../css/Diary.module.css';
 import { fetchCommits } from '@/store/slices/githubSlice';
 
 export default function Diary() {
@@ -25,42 +24,59 @@ export default function Diary() {
         const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
         setDaysRemaining(daysRemaining);
     }
+    
+    function getDayRemainingText(){
+        if(daysRemaining >= 0){
+            return `專題發表剩餘 ${daysRemaining} 天`
+        }else{
+            return `專題發表過了 ${Math.abs(daysRemaining)} 天`
+        }
+    }
 
     return (
-        <div className={css.mainContainer}>
-            <div className={css.countDown}>
+        <div className="bg-gray-100 min-h-screen px-8 md:px-32 pb-6">
+            <div className="flex flex-col md:flex-row justify-between py-4 items-center mb-5 border-b border-black text-center text-2xl md:text-2xl">
                 <h1>Github Commit List</h1>
-                <h2>專題發表剩餘 {daysRemaining} 天</h2>
+                <h1>{getDayRemainingText()}</h1>
             </div>
-            <div className={css.commitList}>
+            <div className="w-full">
                 {
                     isDataLoading ?
-                    <div className={css.loading}>
+                    <div className="flex justify-center items-center">
                         <h1>資料載入中...</h1>
                     </div>
                     :
                     commits.map((commit, index) => (
-                        <div key={index} className={css.commitItemContainer}>
-                            <div className={css.commitInfo}>
-                                <div className={css.commitDetails}>
+                        <div key={index} className="w-full flex justify-between mb-2 md:mb-2.5 px-4 py-2 border border-gray-400 rounded-lg">
+                            <div className="flex items-center">
+                                <div className="">
                                     {commit.message.split('\n').map((line, i) => (
                                         <div key={i}>{line}</div>
                                     ))}
-                                    <p>
-                                        <a href={`https://www.github.com/${commit.author}`} target="_blank" rel="noopener noreferrer">
-                                        <img
-                                            src={commit.avatarURL}
-                                            alt={`${commit.author} avatar`}
-                                            width={20}
-                                            height={20}
-                                            className={css.avatar}
-                                        />
-                                        {commit.author}</a> - {new Date(commit.date).toLocaleDateString() + ' ' + new Date(commit.date).toLocaleTimeString()}
-                                    </p>
+                                    <div className="flex flex-col md:flex-row items-start mt-2 md:mt-4 text-gray-500">
+                                        <a href={`https://www.github.com/${commit.author}`} target="_blank" rel="noopener noreferrer" className="font-bold text-black no-underline flex items-center">
+                                            <img
+                                                src={commit.avatarURL}
+                                                alt={`${commit.author} avatar`}
+                                                width={20}
+                                                height={20}
+                                                className="rounded-full mr-1"
+                                            />
+                                            {commit.author}
+                                        </a>
+                                        <p className="hidden md:block px-1">
+                                            -
+                                        </p>
+                                        <p>
+                                            {new Date(commit.date).toLocaleDateString() + ' ' + new Date(commit.date).toLocaleTimeString()}
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                            <div className={css.commitID}>
-                                <a href={commit.commitURL} target="_blank" rel="noopener noreferrer">{"#" + commit.id}</a>
+                            <div className="flex items-center">
+                                <a href={commit.commitURL} target="_blank" rel="noopener noreferrer" className="font-bold text-black no-underline">
+                                    {"#" + commit.id}
+                                </a>
                             </div>
                         </div>
                     ))
