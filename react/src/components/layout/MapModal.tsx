14a84b4963282d1bac52508ae08e941b8889160a
TimmyHung <timmy0707@gmail.com>
2024-09-02 21:42:04 +0800

    修正"歷史紀錄"介面，點開地圖顯示，點擊圖標會導致畫面關閉的問題。

--- react/src/components/layout/MapModal.tsx
+++ react/src/components/layout/MapModal.tsx
@@ -13,14 +13,18 @@ interface MapModalProps {
 const MapModal: React.FC<MapModalProps> = ({ isVisible, textTitle, onClose, geoJsonData, center, zoom = 12 }) => {
   if (!isVisible) return null;
 
+  const handleMapClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-10 flex justify-center items-center z-50" onClick={onClose}>
-      <div className="flex flex-col gap-2 bg-white p-5 rounded-lg shadow-lg w-4/5 h-4/5 relative">
+      <div className="flex flex-col gap-2 bg-white p-5 rounded-lg shadow-lg w-4/5 h-4/5 relative" onClick={handleMapClick}>
         <div className="w-full flex justify-between items-center relative break-words">
           <p className="">
             {textTitle}
           </p>
-          <div className="max-h-8 hover:bg-red-700 border border-black rounded-xl h-full flex justify-center items-center bg-gray-200"onClick={() => onClose()}>
+          <div className="max-h-8 hover:bg-red-700 border border-black rounded-xl h-full flex justify-center items-center bg-gray-200" onClick={() => onClose()}>
               <i className="fa-solid fa-x cursor-pointer px-4 hover:text-white"></i>
           </div>
         </div>
